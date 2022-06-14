ea88b9bfe79da9d4221b7777a0badaeac0708a51
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-14 12:24:22 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+for (let index = 0; index < 5; index++) {
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+    basic.pause(500)
+}
